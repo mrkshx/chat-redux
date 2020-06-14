84590e455b28f2b6187ebcfca2fb93c80bf8c5e7
Markus Mengel <markus.mengelhx@gmail.com>
2020-06-14 13:13:00 +0200

    added 2 two initial messages

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,18 @@ import selectedChannelReducer from './reducers/selected_channel_reducer';
 import currentUserReducer from './reducers/current_user_reducer';
 
 const initialState = {
-  messages: [],
+  messages: [
+    {
+      author: "anonymous92",
+      content: "Hello world!",
+      created_at: "2017-09-26T23:03:16.365Z"
+    },
+    {
+      author: "anonymous77",
+      content: "My name is anonymous77",
+      created_at: "2017-09-26T23:03:21.194Z"
+    }
+  ],
   channels: ['general', 'react', 'berlin'],
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
   selectedChannel: 'react'
